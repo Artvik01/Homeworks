3305ea4a93687c74522bd1f53e8d00cd1ca9bb7c
Artem Munaev <112118692+Artvik01@users.noreply.github.com>
2022-09-11 21:48:30 +0300

    Create HW4 (Lesson_05_09_22).py

---
+++ HW4 (Lesson_05_09_22).py
@@ -0,0 +1,14 @@
+value = 123
+new_value = value / 2 if value < 100 else - value
+#####################
+value = 123
+new_value = 1 if value < 100 else 0
+#####################
+value = 123
+new_value = True if value < 100 else False
+#####################
+my_str = "qwerty"
+new_str = my_str * 2 if len(my_str) < 5 else my_str
+#####################
+my_str = "qwerty"
+new_str = my_str[::-1] if len(my_str) < 5 else my_str
