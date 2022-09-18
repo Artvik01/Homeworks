981d415cf224379fa1323764855443ee0e86d169
Artem Munaev <artvik0121@gmail.com>
2022-09-18 21:30:55 +0300

    Homework

--- HW6 (Lesson_12_09_22).py
+++ HW6 (Lesson_12_09_22).py
@@ -2,17 +2,24 @@
 #for number in my_list:
 #    if number > 100:
 #        print(number)
+
+
 #############################################
+
+
 #my_list = [45, 87, 345, 95, 198, 63]
 #my_results = []
 #for number in my_list:
 #    if number > 100:
 #        my_results.append(number)
 #print(my_results)
+
+
 #############################################
+
+
 #my_list = [23, 454]
 #if len(my_list) < 2:
 #    my_list.append(0)
 #else:
 #    my_list.append(my_list[-1] + my_list[-2])
-#print(my_list)
