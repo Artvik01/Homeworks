76149a0b24878b348850921995d4288ba3901054
Artem Munaev <artvik0121@gmail.com>
2022-10-07 20:16:16 +0300

    Homework v1

--- HW10 (Lesson_29_09_22).py
+++ HW10 (Lesson_29_09_22).py
@@ -1,15 +1,13 @@
-
-
-
 # 1. Написать функцию, которая получает в виде параметра имя файла названия интернет доменов (domains.txt)
 # и возвращает их в виде списка строк (названия возвращать без точки).
 
+
 file_domains = 'materials/domains.txt'
 def read_domain_names (filename):
-    file = open(filename, 'r')
-    data = file.read()
-    data = data.replace('.', '')
-    return data
+    with open(filename, 'r') as file:
+        data = file.read()
+        data = data.replace('.', '').split('\n')
+        return data
 data = read_domain_names(file_domains)
 
 
@@ -20,13 +18,19 @@ data = read_domain_names(file_domains)
 # Каждая строка файла содержит номер, фамилию, страну, некоторое число (таблица взята с википедии).
 # Разделитель - символ табуляции "\t"
 
+
 file_names = 'materials/names.txt'
 def read_only_names (filename):
-    file = open(filename, 'r')
-    data = file.read()
-    data = data.split('\t')[1::3]
-    return data
-data = read_only_names(file_names)
+    result = []
+    with open(filename, 'r') as file:
+        file = file.read()
+        file_data = file.splitlines()
+        for line in file_data:
+                data = line.split('\t')
+                name = data[1]
+                result.append(name)
+    return result
+result = read_only_names (file_names)
 
 
 
@@ -35,3 +39,19 @@ data = read_only_names(file_names)
 # словарей вида {"date": date}
 # в которых date - это дата из строки (если есть),
 # Например [{"date": "1st January 1919"}, {"date": "8th February 1828"},  ...]
+
+
+file_authors = 'materials/authors.txt'
+def read_dates(filename):
+    result = list()
+    file = open(filename, 'r')
+    file = file.read()
+    file_read = file.splitlines()
+    for symbol in file_read:
+        if symbol != '':
+            data = symbol.split('-')
+            dates = data[0]
+            if len(dates.split()) > 2:
+                result.append({'date': dates})
+    return result
+result = read_dates(file_authors)
